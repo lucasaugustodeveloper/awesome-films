c47311325610773126aa2d37b72c76f8e5defc9f
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-01-26 13:33:04 -0200

    adicionado rota do app

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,19 @@
 import '../assets/sass/index.scss'
 
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 import Films from '../Films/film'
 
-const App = props => <Films />
+const About = ({ match }) => <h2>ID: { match.params.id }</h2>
+
+const App = props => (
+  <Router>
+    <div>
+      <Route path="/" exact component={ Films } />
+      <Route path="/detail/:id" component={ About } />
+    </div>
+  </Router>
+)
 
 export default App
--- src/Films/film-item.jsx
+++ src/Films/film-item.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import Genres from './film-genres'
 
 export default props => {
-  const renderFilms = () => {
-    const films = props.films || []
-
-    return films.map(film => (
-      <div className="film_item row" key={film.id}>
-        <div className="film_item_img col-lg-2">
+  return props.films.map(film => (
+    <div className="film_item row" key={film.id}>
+      <div className="film_item_img col-lg-2">
+        <Link to={'/detail/' + film.id}>
           <img
-            src={'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + film.poster_path }
-            alt={ film.title }
+            src={'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + film.poster_path}
+            alt={film.title}
+          />
+        </Link>
+      </div>
+      <div className="film_item_info col-lg-8">
+        <div className="film_item_title">
+          <Link to={'/detail/' + film.id}>
+            {film.title}
+          </Link>
+        </div>
+        <div className="film_item_description">
+          {film.overview}
+        </div>
+        <div className="film_item_genres">
+          <Genres
+            genres={props.genres}
+            genres_ids={film.genre_ids}
           />
         </div>
-        <div className="film_item_info col-lg-8">
-          <div className="film_item_title">
-            { film.title }
-          </div>
-          <div className="film_item_description">
-            { film.overview }
-          </div>
-          <div className="film_item_genres">
-            <Genres
-              genres={ props.genres }
-              genres_ids={ film.genre_ids }
-            />
-          </div>
-        </div>
       </div>
-    ))
-  }
-
-  return renderFilms()
+    </div>
+  ))
 }
